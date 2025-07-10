9392f1e206bebe86d0747a43b5fd829492aed1a0
gurpreet-gif <gurpreetramgarhia80@gmail.com>
2025-07-10 18:50:27 +0530

    Update deployment config and add NotFound.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import NewsWrapper from './components/NewsWrapper';
+import NotFound from './components/NotFound';
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter basename="/V-react-code-newsapp">
+    <BrowserRouter basename="/newsapp">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Navigate to="/newsapp" />} />
-        <Route path="/newsapp" element={<NewsWrapper category="general" />} />
+        <Route path="/" element={<Navigate to="/category/general" />} />
         <Route path="/category/:categoryName" element={<NewsWrapper />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
